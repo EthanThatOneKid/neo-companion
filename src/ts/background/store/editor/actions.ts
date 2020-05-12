d22075337f8996865a70569388e16403f8fcf3b5
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-05-12 15:04:06 -0700

    created editor for redux

--- src/ts/background/store/editor/actions.ts
+++ src/ts/background/store/editor/actions.ts
@@ -1,10 +1,19 @@
 import { Action } from 'redux';
 
 export type EditorActionTypes = 'NEW_LINE' | 'REM_LINE' | 'CHANGE';
-export type EditorPayload = string[];
-
+export interface EditorPayload {
+	value: string[],
+	index?: number,
+	lines?: string[]
+}
 export type EditorActions = Action<EditorActionTypes, EditorPayload>;
 
-export const newLine = (payload: EditorPayload = []) => ({ type: 'NEW_LINE', payload });
-export const removeLine = (payload: EditorPayload = []) => ({ type: 'REM_LINE', payload });
-export const change = (payload: EditorPayload = []) => ({ type: 'CHANGE', payload });
+const defaultEditorPayload: EditorPayload = {
+	value: [],
+	index: -1,
+	lines: []
+};
+
+export const newLine = (payload: EditorPayload = defaultEditorPayload) => ({ type: 'NEW_LINE', payload });
+export const removeLine = (payload: EditorPayload = defaultEditorPayload) => ({ type: 'REM_LINE', payload });
+export const change = (payload: EditorPayload = defaultEditorPayload) => ({ type: 'CHANGE', payload });
--- src/ts/background/store/editor/reducer.ts
+++ src/ts/background/store/editor/reducer.ts
@@ -1,26 +1,40 @@
 import { Reducer} from 'redux';
 import { EditorActions } from './actions';
 
-// export interface ICounter {
-// 	clicksMade: number;
-// }
+export interface IEditor {
+	value: string[]
+}
 
-// const initialState: ICounter = {
-// 	clicksMade: 0
-// };
+const initialState: IEditor = {
+	value: []
+};
 
-// const counter: Reducer<ICounter, CounterActions> = (state = initialState, action) => {
-// 	const { payload } = action;
-// 	switch (action.type) {
-// 		case 'INCREMENT':
-// 			return { ...state, clicksMade: state.clicksMade + (payload || 1) };
+const editor: Reducer<IEditor, EditorActions> = (state = initialState, action: EditorActions) => {
+	const { payload } = action;
+	switch (action.type) {
+		case 'NEW_LINE':
+			if (payload.lines !== undefined && payload.index !== undefined) {
+				payload.value.splice(payload.index, 0, ...payload.lines);
+				const { value } = payload;
+				return { value };
+			}
+			return { ...state };
 
-// 		case 'DECREMENT':
-// 			return { ...state, clicksMade: state.clicksMade - (payload || 1) };
+		case 'REM_LINE':
+			if (payload.index !== undefined) {
+				payload.value.splice(payload.index, 1);
+				const { value } = payload;
+				return { value };
+			}
+			return { ...state };
 
-// 		default:
-// 			return state;
-// 	}
-// };
+		case 'CHANGE':
+			const { value } = payload;
+			return { value };
+		
+		default:
+			return { ...state };
+	}
+};
 
-// export default counter;
+export default editor;
